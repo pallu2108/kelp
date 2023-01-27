032794599e919edab1ef002a4dc111cec721b344
Pallavi Jagtap <101566082+pallu2108@users.noreply.github.com>
2023-01-27 14:01:08 +0530

    App Updated

--- src/index.js
+++ src/index.js
@@ -4,35 +4,27 @@ fs.readFile("src/sample_input/input.txt", "utf8", (err, data) => {
   if (err) {
     throw err;
   }
-  let obj = {};
+  let accounts = {};
   let inputLines = data.toString().split("\n");
   for (let i = 0; i < inputLines.length; i++) {
     input = inputLines[i].trim().split(" ");
     if (input[0] === "CREATE") {
-      if (obj[input[1]] === undefined) {
-        obj[input[1]] = { name: input[2], balance: 0 };
+      if (accounts[input[1]] === undefined) {
+        accounts[input[1]] = { name: input[2], balance: 0 };
       }
     }
     if (input[0] === "DEPOSIT") {
-      if (input[1] === "ACC001") {
-        obj["ACC001"].balance = obj["ACC001"].balance + Number(input[2]);
-      }
-      if (input[1] === "ACC002") {
-        obj["ACC002"].balance = obj["ACC002"].balance + Number(input[2]);
-      }
+      accounts[input[1]].balance =
+        accounts[input[1]].balance + Number(input[2]);
     }
     if (input[0] === "WITHDRAW") {
-      if (input[1] === "ACC001") {
-        obj["ACC001"].balance = obj["ACC001"].balance - Number(input[2]);
-      }
-      if (input[1] === "ACC002") {
-        obj["ACC002"].balance = obj["ACC002"].balance - Number(input[2]);
-      }
+      accounts[input[1]].balance =
+        accounts[input[1]].balance - Number(input[2]);
     }
-    if(input[0] === "BALANCE"){
-        let name = obj[input[1]].name;
-        let balance = obj[input[1]].balance
-        console.log(`${name} ${balance}`)
+    if (input[0] === "BALANCE") {
+      let name = accounts[input[1]].name;
+      let balance = accounts[input[1]].balance;
+      console.log(`${name} ${balance}`);
     }
   }
 });
